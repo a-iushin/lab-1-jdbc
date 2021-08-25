541f48a768578328b1a6b66fd59aa49dbda3c7ce
Andrei Iushin <a.iushin@haulmont.com>
2021-08-26 00:10:56 +0300

    Initial commit

---
+++ src/main/java/com/luxoft/springdb/lab1/dao/CountryNotFoundException.java
@@ -0,0 +1,10 @@
+package com.luxoft.springdb.lab1.dao;
+
+public class CountryNotFoundException extends Exception {
+
+	private static final long serialVersionUID = 1L;
+
+	public CountryNotFoundException(String s){
+		super(s);
+	}
+}
